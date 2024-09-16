f3c701481d8b19c488e3400ca22a91a0c325accb
arghadwip23 <arghadwip23@gmail.com>
2024-09-16 23:21:07 +0530

    initialising the app

---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav className='bg-white w-screen flex justify-between px-10 py-5'>
+ <h1 className='text-black font-bold'>oue app </h1>
+ <button className='btn bg-blue-500 text-2xl p-1 rounded hover:bg-blue-300 hover:text-black'>log in </button>
+    </nav>
+  )
+}
